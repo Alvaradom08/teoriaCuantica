455d7926e8e3062856b5371e25bc22d0d4a27d38
juan mateo alvarado <juan.alvarado-m@escuelaing.edu.co>
2022-09-28 12:20:57 -0500

    parte 2

--- calculadoraMatr.py
+++ calculadoraMatr.py
@@ -5,7 +5,7 @@ def adicionVec(vc1,vc2):
         ans = ans + [cplx.sumacplx(vc1[i],vc2[i])]
     return r
 def transMtrx(mtrx):
-    pass
+    return [[mtrx[j][i]for j in range(len(mtrx))]for i in range(len(mtrx[0]))]
 def conjugadoMtrx(mtrx):
     return [[cplx.conjcplx(mtrx[i][j])for j in range(len(mtrx[0]))]for i in range(len(mtrx))]
 def adjuntaMtrx(mtrx):
@@ -13,7 +13,7 @@ def adjuntaMtrx(mtrx):
 
 def verificarMult(mt1,mt2):
     r= True
-    if len(mt1)!=len(mt2)
+    if len(mt1)!=len(mt2):
         t=False
     return r
 def productoMtrx(mt1,mt2):
@@ -21,10 +21,31 @@ def productoMtrx(mt1,mt2):
         r=[[(0,0) for j in range(len(mt1[0]))]for i in range(len(mt1))]
         for i in range(len(mt1)):
             for j in range(len(mt2[0])):
-                for k in range(len(mt2))
+                for k in range(len(mt2)):
                     r[i][j]=cplx.sumacplx(cplx.multcplx(mt1[i][k],mt2[k][j]),r[i][j])
     else:
         r="Indefinida"
     return r
 def productoInt(vc1,vc2):
     return productoMtrx(adjuntaMtrx(vc1),mt2)[0][0]
+def normaVc (vc):
+    return (productoInt(vc,vc)[0])**(1/2)
+def mtrxhermitiana(mtrx):
+    if adjuntaMtrx(mtrx)== mtrx:
+        res=True
+    else:
+        res=False
+    return res
+def escMultMtrx(esc,mtrx):
+    return [[cplx.multcplx(esc, mtrx[i][j])for j in range(len(mtrx[0]))] for i in range (len(a))]
+
+def mtrxIdentidad (n):
+    m = [[(0.0,0.0) for j in range(len(n))] for i in range(len(n))]
+    for i in range(len(n)):
+        m[i][j] = (1.0,0.0)
+    return m
+"""def verAdd (mtrx1,mtrx2):
+    ver=True
+    if len(mtrx1)!=
+def addMtrx ()
+"""
--- simulador.py
+++ simulador.py
@@ -1,2 +1,21 @@
 import calculadoraCplx as cplx
-import calculadoraMatr as Matx
+import calculadoraMatr as matx
+import numpy as np
+import math
+
+"""funcion para calcular la probabilidad de encontar un particula en una posicion"""
+def probPosicion (pos, vcket):
+    return(cplx.modcplx(vcket[pos][0])**2)/matx.normaVc(vcket)**2
+"""faltana cosas"""
+
+def proba (vc):
+    return cplx.modcplx(vc)**2
+def variEspera (mtrx,  ket):
+    if matx.mtrxhermitiana(mtrx):
+        bra = matx.adjuntaMtrx(matx.productoMtrx(mtrx,ket))[0]
+        for i in range(len(bra)):
+            bra[i]=[bra[i]]
+        mu=matx.productoMtrx(matx.transMtrx(bra),key)[0][0]
+        x = matx.escMultMtrx(mu,matx.mtrxIdentidad(len(mtrx)))
+        y = matx.ad
+
